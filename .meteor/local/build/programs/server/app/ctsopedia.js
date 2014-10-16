1880a8aabf826df758954327c8f4af7c1c355481
David Doogs <ddoogs@opinionlab.com>
2014-10-16 14:53:54 -0500

    Adding logo

---
+++ .meteor/local/build/programs/server/app/ctsopedia.js
@@ -0,0 +1,17 @@
+(function(){if (Meteor.isClient) {
+  Template.search.events({
+    'click button': function(e){
+        console.log("performing search for: ", Meteor.user().profile);
+        e.preventDefault();
+        Router.go('resultsPage');
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+})();
---
+++ .meteor/local/build/programs/web.browser/app/client/routes.js
@@ -0,0 +1,13 @@
+(function(){Router.map(function() {
+  this.route("search", {
+    path: "/"
+  });
+  this.route("resultsPage", {
+    path: "/results"
+  });
+  this.route("clients", {
+    path: "/clients"
+  });
+});
+
+})();
