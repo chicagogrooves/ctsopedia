6a2e1de13c2fa46562e5e5c3381900afbcaae52a
Dean Radcliffe <dradcliffe@opinionlab.com>
2014-10-16 15:49:11 -0500

    google auth tweak

--- ctsopedia.js
+++ ctsopedia.js
@@ -1,12 +1,16 @@
 if (Meteor.isClient) {
 
+  //http://stackoverflow.com/questions/14227222/unauthorized-access-to-google-calendar-api-post-request
   Accounts.ui.config({
     'requestPermissions': {
-      'google' : [
+      'google': [
         'https://www.googleapis.com/auth/drive',
         'https://www.googleapis.com/auth/drive.file',
         'https://www.googleapis.com/auth/userinfo.email'
       ]
+    },
+    'requestOfflineToken': {
+      'google': true
     }
   });
 
