aedc0eb0bcdc7ddbafba42190117507fb7533a7c
Dean Radcliffe <dradcliffe@opinionlab.com>
2014-10-17 05:22:43 -0500

    meteorified, with actual google results working !

--- client/views/google/google.js
+++ client/views/google/google.js
@@ -1,6 +1,44 @@
-Template.google.created = function(){
-  this.googleApi = {"TODO": "file data"};
+//this variable will hold the preauthorized google drive api object
+var driveApi;
+
+var throttledSearch = _.throttle(function(e, tmpl){
+  var term = $(tmpl.find("input.gSearch")).val();
+  console.log("Searching " + term + " ..");
+
+  driveApi.files.list({
+    q: "title contains '" + term.replace("'", "") + "'"
+  }).execute( updateUi );
+}, 250, {leading: false});
+
+function updateUi(results){
+  var docTitles = _.pluck(results.items, "title");
+  Session.set("gdrive-results", results.items);
+  console.log("Inspect window.lastSearchResults for data");
+  window.lastSearchResults = results.items;
+}
+
+Template.gSearch.rendered = function(){
+  //console.log("The googs says:", this.googleApi);
+  $(document).on("gdriveready", function(e, api){
+    console.debug("Ready to query Google Drive files (google page)..");
+    driveApi = api; //give this to the file-global variable
+  });
 };
-Template.google.rendered = function(){
-  console.log("The googs says:", this.googleApi);
-};
+
+Template.gSearch.events({
+  "keyup .gSearch": throttledSearch
+});
+Template.gResults.helpers({
+  "results": function(){
+    return Session.get("gdrive-results");
+  },
+  "lastModifiedPhoto": function(){
+    return "";
+  },
+  "ownerPhoto": function(){
+    if( ! this.lastModifyingUser.picture ){
+      return "";
+    }
+    return this.lastModifyingUser.picture.url;
+  }
+});
